3b46ee982fe25bca757ddeaa54f53c23ccb112fb
shouhei-yamamoto <elefish19920509@gmail.com>
2022-02-25 18:38:03 +0900

    ログインシステム10まで

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン成功した場合
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
   end
 end

301999a1aefd52c10f67271b2818f3883901ee84
shouhei-yamamoto <elefish19920509@gmail.com>
2022-03-01 18:35:47 +0900

    お気に入り機能実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,6 +8,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs/1 or /blogs/1.json
   def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
 
   # GET /blogs/new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_063934) do
+ActiveRecord::Schema.define(version: 2022_03_01_083553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2022_03_01_063934) do
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "blog_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["blog_id"], name: "index_favorites_on_blog_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -34,4 +43,6 @@ ActiveRecord::Schema.define(version: 2022_03_01_063934) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "favorites", "blogs"
+  add_foreign_key "favorites", "users"
 end
